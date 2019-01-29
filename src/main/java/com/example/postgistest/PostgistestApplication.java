fa1f4271f5937345d3bcb6f690646bef30a73216
Qantrous <ikant14@freeuni.edu.ge>
2019-01-29 15:57:29 +0400

    first commit

---
+++ src/main/java/com/example/postgistest/PostgistestApplication.java
@@ -0,0 +1,14 @@
+package com.example.postgistest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgistestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostgistestApplication.class, args);
+    }
+
+}
+
