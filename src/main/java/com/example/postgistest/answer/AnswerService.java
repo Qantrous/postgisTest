fc31dcb6b248f594936ad3cb45103fdb44b7065a
Qantrous <ikant14@freeuni.edu.ge>
2019-07-02 13:36:18 +0400

    removed post construct

--- src/main/java/com/example/postgistest/answer/AnswerService.java
+++ src/main/java/com/example/postgistest/answer/AnswerService.java
@@ -26,37 +26,4 @@ public class AnswerService {
         return answers.get(random.nextInt(answers.size())).getAnswer();
 
     }
-
-    @PostConstruct
-    public void generateData() {
-        List<Answer> answers = new ArrayList<>();
-
-        answers.add(new Answer("I love you"));
-        answers.add(new Answer("I miss you"));
-        answers.add(new Answer("I’m thinking about you"));
-        answers.add(new Answer("I adore you"));
-        answers.add(new Answer("You are my everything"));
-        answers.add(new Answer("You take my breath away"));
-        answers.add(new Answer("You make my smile"));
-        answers.add(new Answer("I’m yours"));
-        answers.add(new Answer("I’m always by your side"));
-        answers.add(new Answer("I’ll love you always"));
-        answers.add(new Answer("Remember the good times"));
-        answers.add(new Answer("You’re worth every mile between us"));
-        answers.add(new Answer("We’ll see each other soon"));
-        answers.add(new Answer("Love conquers distance"));
-        answers.add(new Answer("No regrets, just love"));
-        answers.add(new Answer("send nudes"));
-        answers.add(new Answer("send nudes"));
-        answers.add(new Answer("wona fak?"));
-        answers.add(new Answer("wona fak?"));
-        answers.add(new Answer("No regrets, just love"));
-        answers.add(new Answer("No regrets, just love"));
-        answers.add(new Answer("No regrets, just love"));
-
-
-
-
-        answers.forEach(answerRepository::saveAndFlush);
-    }
 }
